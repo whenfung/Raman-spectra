57f4f25c8089e0ceb588c19e7b840204b6257bc9
JiangChaojie <1281104370@qq.com>
2019-06-05 19:00:53 +0800

    添加峰值存储pks

--- src/plotdemo.m
+++ src/plotdemo.m
@@ -1,8 +1,13 @@
-clear;clc
+clear;clc;close all;
 [data1,name1,num1] = readall_txt('..\data\试剂标准品谱图');
 [data2,name2,num2] = readall_txt('..\data\混合样测试');
+num = 1;
 for i = 1:num1
     temp = cell2mat(data1(i));
+    [~,loc] = findpeaks(temp(:,2));
+    [dim,~] = size(loc);
+    pks(num) = mat2cell(loc,dim,1);
+    num = num + 1;
     plot(temp(:,1),temp(:,2))
     name1(i) = strrep(name1(i),'.txt','');
     hold on;
@@ -14,6 +19,10 @@ legend(name1);
 
 for i = 1:num2
     temp = cell2mat(data2(i));
+    [~,loc] = findpeaks(temp(:,2));
+    [dim,~] = size(loc);
+    pks(num) = mat2cell(loc,dim,1);
+    num = num + 1;
     name2(i) = strrep(name2(i),'.txt','');
     figure;
     plot(temp(:,1),temp(:,2),'DisplayName',char(name2(i)));
@@ -21,7 +30,8 @@ for i = 1:num2
     for j = 1:num1
         if(contains(name2(i),name1(j)))
             temp = cell2mat(data1(j));
-            plot(temp(:,1),temp(:,2),'DisplayName',char(name1(j)))
+            plot(temp(:,1),temp(:,2),'DisplayName',char(name1(j)));
+            [~,loc] = findpeaks(temp(:,2));
         end
     end
     xlabel('波数');
