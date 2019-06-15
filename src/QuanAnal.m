bc6e722a60e4f5656173a4889a816164aae04516
JiangChaojie <1281104370@qq.com>
2019-06-15 12:21:37 +0800

    定量分析

---
+++ src/QuanAnal.m
@@ -0,0 +1,33 @@
+clear;clc;close all;
+[data1,name1,num1] = readall_txt('..\data\试剂标准品谱图');
+[data2,name2,num2] = readall_txt('..\data\混合样测试');
+name1 = strrep(name1,'.txt','');
+name2 = strrep(name2,'.txt','');
+% for i = 1:num1
+%     plot(data1{i}(:,1),data1{i}(:,2));
+%     hold on;
+% end
+% title('拉曼光谱(标准)');
+% xlabel('波数');
+% ylabel('光强');
+% legend(name1);
+
+for i = 1:num2
+    figure;
+    plot(data2{i}(:,1),data2{i}(:,2),'DisplayName',name2{i});
+    hold on;
+    sum = zeros(1601,1);
+    for j = 1:num1
+        if(contains(name2{i},name1{j}))
+%             plot(data1{j}(:,1),data1{j}(:,2),'DisplayName',name1{j});
+            k = strfind(name2{i},name1{j});
+            a = str2num(name2{i}(k+length(name1{j}):k+1+length(name1{j})))/100;
+            sum = sum + data1{j}(:,2)*a;
+%             plot(data1{j}(:,1),data1{j}(:,2)*a,'DisplayName',strcat(name1{j},num2str(a)));
+        end
+    end
+    plot(data1{j}(:,1),sum,'DisplayName','混合后');
+    xlabel('波数');
+    ylabel('光强');
+    legend;
+end
--- src/peakDemo.m
+++ src/peakDemo.m
@@ -14,11 +14,11 @@ legend(name1);
 for i = 1:num2
     name2(i) = strrep(name2(i),'.txt','');
     figure;
-    plot(data2{i}(:,1),data2{i}(:,2),'DisplayName',char(name2(i)));
+    plot(data2{i}(:,1),data2{i}(:,2),'DisplayName',name2{i});
     hold on;
     for j = 1:num1
         if(contains(name2(i),name1(j)))
-            plot(data1{j}(:,1),data1{j}(:,2),'DisplayName',char(name1(j)));
+            plot(data1{j}(:,1),data1{j}(:,2),'DisplayName',name1{j});
         end
     end
     xlabel('²¨Êý');
